e7f37aa6cf0d6442e0aeecee3779351bad877cf3
Praniti Parmar <pranitip111@gmail.com>
2021-12-20 18:42:18 +0530

    Initial commit

---
+++ printz.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class printz {
+
+    public static void main(String[] args) 
+    {
+        // Write your code here
+        System.out.println("*****");
+        System.out.println("   * ");
+        System.out.println("  *  ");
+        System.out.println(" *   ");
+        System.out.println("*****");
+
+    }
+}
