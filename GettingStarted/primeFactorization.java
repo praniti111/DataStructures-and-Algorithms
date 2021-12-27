949e313394b12f642f91227a3345eb3d90e0cae0
Praniti Parmar <pranitip111@gmail.com>
2021-12-27 17:21:13 +0530

    Create primeFactorization.java

---
+++ GettingStarted/primeFactorization.java
@@ -0,0 +1,29 @@
+//12. Prime Factorization of a number
+
+import java.util.*;
+public class Main
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    
+    primeFact(n);
+  }
+  
+  public static void primeFact(int n)
+  {
+    for(int div=2; div*div<=n;div++)
+    {
+     while(n%div==0)
+     {
+       n = n/div;
+       System.out.print(div + " ");
+     }
+    }
+    if(n!=1)
+    {
+      System.out.print(n);
+    }
+  }
+}
