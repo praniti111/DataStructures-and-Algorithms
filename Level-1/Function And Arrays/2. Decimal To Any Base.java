62f35a1199100dca2c8dd6664edf5a12143bec93
Praniti Parmar <pranitip111@gmail.com>
2022-01-03 01:50:31 +0530

    Create 2. Decimal To Any Base.java

---
+++ Level-1/Function And Arrays/2. Decimal To Any Base.java
@@ -0,0 +1,35 @@
+// 2. Decimal To Any Base
+//1. You are given a decimal number n.
+// 2. You are given a base b.
+// 3. You are required to convert the number n into its corresponding value in base b.
+
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      int b = scn.nextInt();
+      int dn = getValueInBase(n, b);
+      System.out.println(dn);
+   }
+  
+   public static int getValueInBase(int n, int b)
+   {
+       int res = 0;
+       int multiplier = 1;
+    
+      while(n>0)
+      {
+        int divisor = n/b;  
+        int rem = n%b;
+
+        n = divisor;
+
+        res = res + rem*multiplier;
+        multiplier *= 10;
+      }
+      return res;
+   }
+  }
