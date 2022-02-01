adcc847599906eebcd7d0fec961fc90899d89df5
Praniti Parmar <pranitip111@gmail.com>
2022-02-01 23:39:18 +0530

    Update 8.Saddle Price.java

--- Level-1/2D-Arrays/8.Saddle Price.java
+++ Level-1/2D-Arrays/8.Saddle Price.java
@@ -1,4 +1,6 @@
-//Saddle Price
+//8. Saddle Price
+
+//3. The saddle price is defined as the ""least price in the row but the maximum price in the column of the matrix.""
 
 import java.io.*;
 import java.util.*;
@@ -12,7 +14,7 @@ public class Main {
 
         int[][] mat = new int[n][n];
 
-        for(int i = 0;i<n;i++)
+        for(int i = 0;i<n;i++)// Matrix input
         {
             for(int j = 0;j<n;j++)
             {
@@ -20,29 +22,30 @@ public class Main {
             }
         }
 
-        for(int i=0;i<mat.length;i++)
+        for(int i=0;i<mat.length;i++)// Run the loop for the rows 
         {
-            int minCol = 0;
+            //To find the minimum element in the row
+            int minCol = 0;// traverse the columns of that row
 
-            for(int j=0;j<mat[0].length;j++)
+            for(int j=0;j<mat[0].length;j++)// Traversing the columns
             {
-                if(mat[i][j]<mat[i][minCol])
+                if(mat[i][j]<mat[i][minCol])//Check if the first element is less than the element of the same row but next column
                 {
-                    minCol = j;
+                    minCol = j;// if yes the update the column to the minCol to maintain the minimum Column(ie. min elemnt on the row is found as all the number r unique)
                 }
             }
 
-            int maxRow = 0;
-            for(int k = 0;k<mat.length;k++)
+            int maxRow = 0;// To get the maximum in the Column so traverse along the row
+            for(int k = 0;k<mat.length;k++)// Traversing along the row to find the max in col
             {
-                if(mat[k][minCol]>mat[maxRow][minCol])
+                if(mat[k][minCol]>mat[maxRow][minCol])// Check if in that col along the row if the element grater than the tarversing elemnt
                 {
-                    maxRow = k;
+                    maxRow = k;//if yes update maxCol to k
                 }
             }
 
         
-        if(maxRow == i)
+        if(maxRow == i)// if the number of iterations is same as the current MaxRow then print the element of the matrix with the last maxRow and minCol
         {
             System.out.println(mat[maxRow][minCol]);
             return;
@@ -50,7 +53,7 @@ public class Main {
 
         }
 
-    System.out.println("Invalid input");
+    System.out.println("Invalid input");// The loop doesnt satisfy the conditions then it will come out and print for invalid output
     }
 
 }
