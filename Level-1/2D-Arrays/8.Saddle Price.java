78eff6f67247dbd7fe7e5f862228d68e4a25c1a1
Praniti Parmar <pranitip111@gmail.com>
2022-02-01 20:59:00 +0530

    Update 8.Saddle Price.java

--- Level-1/2D-Arrays/8.Saddle Price.java
+++ Level-1/2D-Arrays/8.Saddle Price.java
@@ -1,1 +1,48 @@
+import java.io.*;
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[][] mat = new int[n][n];
+
+        for(int i = 0;i<n;i++)
+        {
+            mat[i][i] = sc.nextInt();
+        }
+
+        for(int i=0;i<mat.length;i++)
+        {
+            int minCol = 0;
+            for(int j=0;j<mat[0].length;j++)
+            {
+                if(mat[i][j]<mat[i][minCol])
+                {
+                    minCol = j;
+                }
+            }
+
+            int maxRow = 0;
+            for(int k = 0;k<mat.length;k++)
+            {
+                if(mat[k][minCol]>mat[minRow][maxCol])
+                {
+                    minRow = k;
+                }
+            }
+
+        }
+        if(maxRow == i)
+        {
+            System.out.print(mat[minRow][maxCol]);
+            return;
+        }
+
+    }
+    System.out.println("Invalid input");
+
+}
