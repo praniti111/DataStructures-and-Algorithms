e355e01c4f1298f7594f46048bfb40d1d6bb63e1
Praniti Parmar <pranitip111@gmail.com>
2022-02-02 21:55:58 +0530

    Update 9. Ring Rotate.java

--- Level-1/2D-Arrays/9. Ring Rotate.java
+++ Level-1/2D-Arrays/9. Ring Rotate.java
@@ -1,1 +1,3 @@
-
+//9. Ring Rotate 
+//You are given a n*m matrix where n are the number of rows and m are the number of columns. You are also given n*m numbers representing the elements of the matrix.
+//You will be given a ring number 's' representing the ring of the matrix. For details, refer to image.
