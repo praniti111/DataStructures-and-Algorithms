feccab1792390cd4d7e825c38f4e13d89a39f542
Praniti Parmar <pranitip111@gmail.com>
2022-03-31 20:40:57 +0530

    Create 1. Print Decreasing.java

---
+++ Level-1/Recursion/Introduction To Recursion/1. Print Decreasing.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{ 
+
+public static void main(String[] args)
+{
+  Scanner sc = new Scanner(System.in);
+  int n = sc.nextInt();
+  printDecreasing(n);
+}
+
+public static void printDecreasing(int n)
+{
+  //Base case: Gives condition where the recursion terminates
+  if(n == 0) return;
+  
+  //Expectation meeting with faith
+  System.out.println(n);
+  
+  //Faith: This is where the function calls itself and has a faith that it will complete the further function call and return to the function which has called it
+  printDecreasing(n-1);
+}
+}
