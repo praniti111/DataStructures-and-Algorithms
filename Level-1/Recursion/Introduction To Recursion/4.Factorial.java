95272b95b303dd4956d5aeebbb1b0f450c73eb1e
Praniti Parmar <pranitip111@gmail.com>
2022-04-02 02:35:35 +0530

    Update 4.Factorial.java

--- Level-1/Recursion/Introduction To Recursion/4.Factorial.java
+++ Level-1/Recursion/Introduction To Recursion/4.Factorial.java
@@ -1,1 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int f = factorial(n);
+    System.out.println(f);
+  }
+  
+  public static int factorial(int n)
+  {
+    //Base Case
+    if(n==1) return 1;
+    
+    //Faith
+    int ans = n * factorial(n-1);
+    
+    //Expectation meeting faith
+    return ans;
+  }
+}
 
