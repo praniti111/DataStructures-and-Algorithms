d2d79da1bec04491a74aaaaf9d32c2936b72c7e1
Praniti Parmar <pranitip111@gmail.com>
2022-08-12 21:54:39 +0530

    Create Test 2  Code1.java

---
+++ Test Solutions/Amazon She Codes/Test 2  Code1.java
@@ -0,0 +1,18 @@
+/* Code 1: Amazon Shopping provides a product-search feature that makes browsing
+
+*/
+import java.util.*;
+import java.io*;
+
+class Result{
+  public static int findMinimumCharacters(String searchWord, String resultWord){
+    int j=0;
+    for(int i=0;i<searchWord.length();i++){
+      if(j==resultWord.length()) break;
+      if(searchWord.charAt(i)==resultWord.charAt(j)){
+        j++;
+      }
+    }
+    return resultWord.length()-j;
+  }
+}
