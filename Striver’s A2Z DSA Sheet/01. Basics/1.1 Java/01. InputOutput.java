140d07d28316a4dd380b9969767baf6473d51762
Praniti Parmar <pranitip111@gmail.com>
2023-07-17 21:41:02 +0530

    Update 01. InputOutput.java

--- Striver’s A2Z DSA Sheet/01. Basics/1.1 Java/01. InputOutput.java
+++ Striver’s A2Z DSA Sheet/01. Basics/1.1 Java/01. InputOutput.java
@@ -1,1 +1,21 @@
+//Q. https://www.codingninjas.com/studio/problems/find-character-case_58513?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
 
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    char ch = sc.next().charAt(0);
+
+    if(ch>='A' && ch<='Z'){
+      System.out.println(1);
+    }
+    else if(ch>='a' && ch<='z'){
+      System.out.println(0);
+    }
+    else{
+      System.out.println(-1);
+    }
+    
+  }
+}
