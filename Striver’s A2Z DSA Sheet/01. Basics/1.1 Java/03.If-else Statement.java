491ec5a0d54b0668f4ff73881c45a307a4a7e11f
Praniti Parmar <pranitip111@gmail.com>
2023-08-18 13:20:58 +0530

    Create 03.If-else Statement.java

---
+++ Striver’s A2Z DSA Sheet/01. Basics/1.1 Java/03.If-else Statement.java
@@ -0,0 +1,16 @@
+// Q: https://www.codingninjas.com/studio/problems/if-else-decision-making_8357235?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=0
+
+import java.util.Scanner;
+
+public class Solution{
+ public static String compareIfElse(int a, int b) {
+        // Write your code here
+        if(a>b){
+          return "greater";
+        }
+        else if(a<b){
+          return "smaller";
+        }
+   return "equal";
+  }
+}
